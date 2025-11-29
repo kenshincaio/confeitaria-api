ccb59960a6fcf94614b3085374d19092333a7f74
yamada.caiokenshin <yamada.caiokenshin@gmail.com>
2025-11-29 00:39:52 +0000

    news

--- cardapio.py
+++ cardapio.py
@@ -2,19 +2,13 @@ from conexao import get_conexao
 from psycopg2.extras import RealDictCursor
 from flask import jsonify
 
+
+
 def get_cardapio():
-    cardapio = [
-        {
-            'id': 1,
-            'titulo': 'Bolo de Chocolate',
-            'descricao': 'Delicioso bolo de chocolate',
-            'foto': ''
-        },
-        {
-            'id': 2,
-            'titulo': 'Bolo de Morango',
-            'descricao': 'Delicioso bolo de morango',
-            'foto': ''
-        }
-    ]
-    return cardapio
+    conn = get_conexao()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("SELECT * FROM cardapio")
+    cardapio = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(cardapio)
